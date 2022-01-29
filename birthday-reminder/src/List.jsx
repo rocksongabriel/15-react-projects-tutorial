c10cd7bbc2dd2810fb7d2ee48d4cb0741d48ea0e
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-01-29 14:11:16 +0000

    Complete the birthday reminder project 1/15

---
+++ birthday-reminder/src/List.jsx
@@ -0,0 +1,21 @@
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person
+        return (
+          <div key={id} className="person-card">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+
+export default List
